0241bf7747cbb57ed53635f80cbe437809a578c5
Daniel Ricklin <ricklin.daniel@outlook.fr>
2024-04-23 16:23:32 +0200

    beginning firewall show page, request done

--- app/services/firewall_service.ts
+++ app/services/firewall_service.ts
@@ -24,11 +24,19 @@ export default class FirewallService {
         let vdoms = await this.getData(firewall, '/system/vdom')
         this.config = vdoms
 
-        this.config.forEach(async (vdom, index) => {
-            let interfaces = await this.getData(firewall, `/system/interface?vdom=${vdom.name}`)
-            this.config[index].interface = interfaces
+        return new Promise((resolve) => {
+            this.config.forEach(async (vdom, index) => {
+                this.config[index].interface = await this.getData(firewall, `/system/interface?vdom=${vdom.name}`)
+                this.config[index].service = await this.getData(firewall, `/firewall.service/custom?vdom=${vdom.name}`)
+                this.config[index].service_group = await this.getData(firewall, `/firewall.service/group?vdom=${vdom.name}`)
+                this.config[index].address = await this.getData(firewall, `/firewall/address?vdom=${vdom.name}`)
+                this.config[index].address_group = await this.getData(firewall, `/firewall/addrgrp?vdom=${vdom.name}`)
+                this.config[index].vip = await this.getData(firewall, `/firewall/vip?vdom=${vdom.name}`)
+                this.config[index].vip_group = await this.getData(firewall, `/firewall/vipgrp?vdom=${vdom.name}`)
+                this.config[index].ippool = await this.getData(firewall, `/firewall/ippool?vdom=${vdom.name}`)
+                this.config[index].policy = await this.getData(firewall, `/firewall/policy?vdom=${vdom.name}`)
+                resolve(this.config)
+            })
         })
-
-        return this.config
     }
 }
